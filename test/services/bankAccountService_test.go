108424211075e9de57805e02750bff40cc2cdd44
Syubban Fakhriya <syubbanfakhriya@gmail.com>
2024-03-13 22:42:54 +0700

    feat: adjust create bank account service test

--- test/services/bankAccountService_test.go
+++ test/services/bankAccountService_test.go
@@ -18,101 +18,103 @@ func beforeEach(t *testing.T) (sqlmock.Sqlmock, *sql.DB) {
 		t.Fatal("Error creating mock database connection:", err)
 	}
 
-	return mock, db;
+	return mock, db
 }
 
-func TestShouldReturnBankAccountResponseAndErrorNil_whenInsertSuccess(t *testing.T) {
-	var mock, db = beforeEach(t)
-	mockBankName := "MockBank"
-	mockAccountName := "MockAccount"
-	mockAccountNumber := "1234567890"
-	mockUserId := 1
-	mock.ExpectPrepare("INSERT INTO bank_accounts").
-		ExpectExec().
-		WithArgs(mockUserId, mockBankName, mockAccountName, mockAccountNumber).
-		WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectQuery("SELECT LASTVAL()").
-		WillReturnRows(sqlmock.NewRows([]string{"account_id"}).AddRow(1))
+func TestCreateBankAccount(t *testing.T) {
+	t.Run("ShouldReturnBankAccountResponseAndErrorNil_whenInsertSuccess", func(t *testing.T) {
+		var mock, db = beforeEach(t)
+		mockBankName := "MockBank"
+		mockAccountName := "MockAccount"
+		mockAccountNumber := "1234567890"
+		mockUserId := 1
+		mock.ExpectPrepare("INSERT INTO bank_accounts").
+			ExpectExec().
+			WithArgs(mockUserId, mockBankName, mockAccountName, mockAccountNumber).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectQuery("SELECT LASTVAL()").
+			WillReturnRows(sqlmock.NewRows([]string{"account_id"}).AddRow(1))
 
-	// Call the function under test
-	response, err := services.CreateBankAccount(mockUserId, bankaccount.BankAccountRequest{
-		BankName:          mockBankName,
-		BankAccountName:   mockAccountName,
-		BankAccountNumber: mockAccountNumber,
-	}, db)
+		// Call the function under test
+		response, err := services.CreateBankAccount(mockUserId, bankaccount.BankAccountRequest{
+			BankName:          mockBankName,
+			BankAccountName:   mockAccountName,
+			BankAccountNumber: mockAccountNumber,
+		}, db)
 
-	// Check for errors
-	assert.NoError(t, err)
-	assert.NotNil(t, response)
-	assert.Equal(t, 1, response.AccountID)
-	assert.Equal(t, mockBankName, response.BankName)
-	assert.Equal(t, mockAccountName, response.AccountName)
-	assert.Equal(t, mockAccountNumber, response.AccountNumber)
-	assert.Equal(t, 1, response.UserID)
-	assert.WithinDuration(t, time.Now(), response.CreatedAt, 1*time.Second)
-	assert.WithinDuration(t, time.Now(), response.UpdatedAt, 1*time.Second)
+		// Check for errors
+		assert.NoError(t, err)
+		assert.NotNil(t, response)
+		assert.Equal(t, "1", response.AccountID)
+		assert.Equal(t, mockBankName, response.BankName)
+		assert.Equal(t, mockAccountName, response.AccountName)
+		assert.Equal(t, mockAccountNumber, response.AccountNumber)
+		assert.Equal(t, "1", response.UserID)
+		assert.WithinDuration(t, time.Now(), response.CreatedAt, 1*time.Second)
+		assert.WithinDuration(t, time.Now(), response.UpdatedAt, 1*time.Second)
 
-	// Ensure all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("Unfulfilled expectations: %s", err)
-	}
-	defer db.Close()
-}
+		// Ensure all expectations were met
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Unfulfilled expectations: %s", err)
+		}
+		defer db.Close()
+	})
 
-func TestShouldReturnBankAccountResponseAndError_whenInsertExecutionFailed(t *testing.T) {
-	var mock, db = beforeEach(t);
-	mockBankName := "MockBank"
-	mockAccountName := "MockAccount"
-	mockAccountNumber := "1234567890"
-	mockUserId := 1
-	mock.ExpectPrepare("INSERT INTO bank_accounts").
-		ExpectExec().
-		WithArgs(mockUserId, mockBankName, mockAccountName, mockAccountNumber).
-		WillReturnError(errors.New("SQL query preparation failed"))
+	t.Run("ShouldReturnBankAccountResponseAndError_whenInsertExecutionFailed", func(t *testing.T) {
+		var mock, db = beforeEach(t)
+		mockBankName := "MockBank"
+		mockAccountName := "MockAccount"
+		mockAccountNumber := "1234567890"
+		mockUserId := 1
+		mock.ExpectPrepare("INSERT INTO bank_accounts").
+			ExpectExec().
+			WithArgs(mockUserId, mockBankName, mockAccountName, mockAccountNumber).
+			WillReturnError(errors.New("SQL query preparation failed"))
+	
+		// Call the function under test
+		response, err := services.CreateBankAccount(mockUserId, bankaccount.BankAccountRequest{
+			BankName:          mockBankName,
+			BankAccountName:   mockAccountName,
+			BankAccountNumber: mockAccountNumber,
+		}, db)
+	
+		assert.Error(t, err)
+		assert.NotNil(t, response)
+	
+		// Ensure all expectations were met
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Unfulfilled expectations: %s", err)
+		}
+		defer db.Close()
+	})
 
-	// Call the function under test
-	response, err := services.CreateBankAccount(mockUserId, bankaccount.BankAccountRequest{
-		BankName:          mockBankName,
-		BankAccountName:   mockAccountName,
-		BankAccountNumber: mockAccountNumber,
-	}, db)
-
-	assert.Error(t, err)
-	assert.NotNil(t, response)
-
-	// Ensure all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("Unfulfilled expectations: %s", err)
-	}
-	defer db.Close()
-}
-
-func TestShouldReturnBankAccountResponseAndError_whenGetLastInsertedValueFailed(t *testing.T) {
-	var mock, db = beforeEach(t);
-	mockBankName := "MockBank"
-	mockAccountName := "MockAccount"
-	mockAccountNumber := "1234567890"
-	mockUserId := 1
-	mock.ExpectPrepare("INSERT INTO bank_accounts").
-		ExpectExec().
-		WithArgs(mockUserId, mockBankName, mockAccountName, mockAccountNumber).
-		WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectQuery("SELECT LASTVAL()").
-		WillReturnError(errors.New("mock QueryRow error"))
-
-	// Call the function under test
-	response, err := services.CreateBankAccount(mockUserId, bankaccount.BankAccountRequest{
-		BankName:          mockBankName,
-		BankAccountName:   mockAccountName,
-		BankAccountNumber: mockAccountNumber,
-	}, db)
-
-	assert.Error(t, err)
-	assert.NotNil(t, response)
-
-	// Ensure all expectations were met
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("Unfulfilled expectations: %s", err)
-	}
-	defer db.Close()
+	t.Run("ShouldReturnBankAccountResponseAndError_whenGetLastInsertedValueFailed", func(t *testing.T) {
+		var mock, db = beforeEach(t)
+		mockBankName := "MockBank"
+		mockAccountName := "MockAccount"
+		mockAccountNumber := "1234567890"
+		mockUserId := 1
+		mock.ExpectPrepare("INSERT INTO bank_accounts").
+			ExpectExec().
+			WithArgs(mockUserId, mockBankName, mockAccountName, mockAccountNumber).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+		mock.ExpectQuery("SELECT LASTVAL()").
+			WillReturnError(errors.New("mock QueryRow error"))
+	
+		// Call the function under test
+		response, err := services.CreateBankAccount(mockUserId, bankaccount.BankAccountRequest{
+			BankName:          mockBankName,
+			BankAccountName:   mockAccountName,
+			BankAccountNumber: mockAccountNumber,
+		}, db)
+	
+		assert.Error(t, err)
+		assert.NotNil(t, response)
+	
+		// Ensure all expectations were met
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Unfulfilled expectations: %s", err)
+		}
+		defer db.Close()
+	})
 }
